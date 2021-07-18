aebbd2e39b49d061ce4bdfed509e9ec5e7f56241
manu.kumar <manu.kumar@globalsign.com>
2021-07-18 20:58:51 +0530

    token implemented

--- in/src/main/java/com/vscca/in/controller/LoginController.java
+++ in/src/main/java/com/vscca/in/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.vscca.in.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +12,15 @@ import com.vscca.in.dto.RequestDto;
 import com.vscca.in.dto.ResponseDto;
 import com.vscca.in.model.LoginTable;
 import com.vscca.in.serivce.LoginService;
+import com.vscca.in.utill.VsccaConstants;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 public class LoginController {
+	
 	
 	@Autowired
 	LoginService loginService;
@@ -24,10 +32,16 @@ public class LoginController {
 			LoginTable loginTable= loginService.findByuserName(requestDto.getUserName());
 		//	System.out.println(loginTable.getUserName());
 			if(requestDto.userName.equals(loginTable.getUserName()) && requestDto.Password.equals(loginTable.getPassword())) {
+				final int EXPIRATIONTIME = 1000*60*60*1*1;// 60 mint
+			String jwtToken = Jwts.builder().setSubject(loginTable.getUserName()).setExpiration(new Date(System.currentTimeMillis()+EXPIRATIONTIME))
+						.claim("roles","user").setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256,VsccaConstants.secretKey).compact();
 				response.setSuccess(200);
+				response.setMessage("success");
+				response.setToken(jwtToken);
 			}
 			else {
 				response.setSuccess(401);
+				response.setMessage("unautherized user");
 			}
 		}
 		
