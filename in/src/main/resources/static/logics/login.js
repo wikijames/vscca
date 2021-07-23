f00478e9f5413d92b752280800fe8efe45056345
wikijames <sharmavivek.1989@gmail.com>
2021-07-23 18:04:31 +0530

    login changes

--- in/src/main/resources/static/logics/login.js
+++ in/src/main/resources/static/logics/login.js
@@ -18,7 +18,13 @@ $(document).ready(function(){
 		
 		$.ajax(settings).done(function (response) {
 		  console.log(response);
-		  window.location = dashboard;
+			if(response.success == 200){
+				window.location = dashboard;
+			}else{
+				
+				alert('try again');
+			}
+		  
 		});
 		
     });
