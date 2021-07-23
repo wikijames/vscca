e9144d70ae14fb4fff3bbead9bfffff3456d483f
wikijames <sharmavivek.1989@gmail.com>
2021-07-23 19:27:07 +0530

    changes in profile page and logic

--- in/src/main/resources/static/logics/profile.js
+++ in/src/main/resources/static/logics/profile.js
@@ -1,23 +1,76 @@
 $(document).ready(function(){
+	$("#responsibility").change(function(){
+	    //alert("checked");
+	    if($(this).is(":checked")){
+	        $(this).val("yes");
+	        //alert($(this).val());
+	    } else {
+	        $(this).val("no");;
+	    }
+	});
+	
+	$("#execution").change(function(){
+	    //alert("checked");
+	    if($(this).is(":checked")){
+	        $(this).val("yes");
+	        //alert($(this).val());
+	    } else {
+	        $(this).val("no");;
+	    }
+	});
+	
+	$("#consulting").change(function(){
+	    //alert("checked");
+	    if($(this).is(":checked")){
+	        $(this).val("yes");
+	        //alert($(this).val());
+	    } else {
+	        $(this).val("no");;
+	    }
+	});
+	
+	$("#intimation").change(function(){
+	    //alert("checked");
+	    if($(this).is(":checked")){
+	        $(this).val("yes");
+	        //alert($(this).val());
+	    } else {
+	        $(this).val("no");;
+	    }
+	});
+	
  // this is the id of the form
 	$("#profile").submit(function(e) {
 	
 	    e.preventDefault(); // avoid to execute the actual submit of the form.
-	
-	    var form = $(this);
-	    var url = form.attr('action');
-	    
-	    $.ajax({
-	           type: "POST",
-	           url: url,
-	           data: form.serialize(), // serializes the form's elements.
-	           success: function(data)
-	           {
-	               alert(data); // show response from the php script.
-	           }
-	         });
-	
-	    
-	});
+		var settings = {
+		    "url": "http://localhost:8080/vscca/userDetails",
+			  "method": "POST",
+			  "timeout": 0,
+			  "headers": {​​​​​​​
+			    "Content-Type": "application/json"
+			},
+		  "data": JSON.stringify({
+		    "firstName": $('#firstName').val(),
+		    "lastName": $('#lastName').val(),
+		    "mobileNumber": $('#mobile').val(),
+		    "emailId": $('#email').val(),
+		    "address": $('#address').val(),
+		    "city": $('#city').val(),
+		    "country": $('#country').val(),
+		    "postalCode": $('#postalCode').val(),
+		    "responsibility": $('#responsibility').val(),
+		    "execution": $('#execution').val(),
+		    "consulting": $('#consulting').val(),
+		    "intimation": $('#intimation').val(),
+		    "aboutMe": $('#aboutMe').val(),
+		    "accessType": $('#accessType').val(),
+		    "location": $('#location').val()
+		  }),
+		}; 
+
+		$.ajax(settings).done(function (response) {
+		  console.log(response);
+		});
 });
 
