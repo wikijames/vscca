d1fe0c0097de9f7dcf28ef95327ba3c34bca8100
wikijames <sharmavivek.1989@gmail.com>
2021-08-29 00:14:03 +0530

    UI formatting

--- in/src/main/resources/static/logics/globalVariables.js
+++ in/src/main/resources/static/logics/globalVariables.js
@@ -2,107 +2,107 @@ var envVar = "http://localhost:8080/vscca/";
 var userRole = sessionStorage.getItem('roles');
 var accessToken = sessionStorage.getItem('token')
 //login
-var postLoginURL = envVar+'login';
+var postLoginURL = envVar + 'login';
 //profile add
-var getAccessURL = envVar+'access';
-var postProfileURL = envVar+'userDetails';
+var getAccessURL = envVar + 'access';
+var postProfileURL = envVar + 'userDetails';
 //profile view
-var getUsersProfileURL = envVar+'users';
-var getUserProfileByIdURL = envVar+'usersById';
-var getLocationURL = envVar+'location';
+var getUsersProfileURL = envVar + 'users';
+var getUserProfileByIdURL = envVar + 'usersById';
+var getLocationURL = envVar + 'location';
 
 //view task
-var getResponsibilityURL = envVar+'taskDetails';
-var getExceutionURL = envVar+'taskDetailsExceution';
-var getIntimationURL = envVar+'taskDetailsIntimation';
-var getConsultingURL = envVar+'taskDetailsConsulting';
+var getResponsibilityURL = envVar + 'taskDetails';
+var getExceutionURL = envVar + 'taskDetailsExceution';
+var getIntimationURL = envVar + 'taskDetailsIntimation';
+var getConsultingURL = envVar + 'taskDetailsConsulting';
 //create task
-var getBillingClientsURL = envVar+'billingClients';
-var getTasksURL = envVar+'tasks';
-var getResponsiblityUsersURL = envVar+'responsiblityUsers';
-var getExecutionUsersURL = envVar+'executionUsers';
-var getConsultingUsersURL = envVar+'consultingUsers';
-var getIntimationUsersURL = envVar+'intimationUsers';
-var postCreateTaskURL = envVar+'createTask';
+var getBillingClientsURL = envVar + 'billingClients';
+var getTasksURL = envVar + 'tasks';
+var getResponsiblityUsersURL = envVar + 'responsiblityUsers';
+var getExecutionUsersURL = envVar + 'executionUsers';
+var getConsultingUsersURL = envVar + 'consultingUsers';
+var getIntimationUsersURL = envVar + 'intimationUsers';
+var postCreateTaskURL = envVar + 'createTask';
 
 // edit view
-var getTaskDetailsByIdURL = envVar+'taskDetailsById';
-var postEditTaskURL = envVar+'editTask';
+var getTaskDetailsByIdURL = envVar + 'taskDetailsById';
+var postEditTaskURL = envVar + 'editTask';
 
-var getLogoutURL = envVar+'logout';
+var getLogoutURL = envVar + 'logout';
 
 
-function checkSession(value){
-	var cookies  = Cookies.get("token");
+function checkSession(value) {
+	var cookies = Cookies.get("token");
 	console.log('cook', cookies);
-	if(cookies == null || cookies == undefined || value == 401 || value == 500){
+	if (cookies == null || cookies == undefined || value == 401 || value == 500) {
 		sessionStorage.clear();
-		window.location = '/vscca';	
+		window.location = '/vscca';
 		alert('Your session has been expired, Please login in again to continue...');
-	}	
+	}
 }
 
 
-function Logout(){
+function Logout() {
 	$.ajax({
-        type: "GET",
-        url:getLogoutURL,
-        dataType: "json",
-        "headers": {
-		    	"Content-Type": "application/json",
-				"Authorization": accessToken	
-		  	},
-			success: function () {
-				if(data.success === 200){
-					sessionStorage.clear();
-					window.location = '/vscca';	
-				}else{
-					sessionStorage.clear();
-					window.location = '/vscca';
-				}
-				
-            },
-			error: function(e){
+		type: "GET",
+		url: getLogoutURL,
+		dataType: "json",
+		"headers": {
+			"Content-Type": "application/json",
+			"Authorization": accessToken
+		},
+		success: function() {
+			if (data.success === 200) {
+				sessionStorage.clear();
+				window.location = '/vscca';
+			} else {
 				sessionStorage.clear();
 				window.location = '/vscca';
 			}
-      });
-		sessionStorage.clear();
-		window.location = '/vscca';
+
+		},
+		error: function(e) {
+			sessionStorage.clear();
+			window.location = '/vscca';
+		}
+	});
+	sessionStorage.clear();
+	window.location = '/vscca';
 };
 
-function topNavUserName(){
+function topNavUserName() {
 	$.ajax({
-        type: "GET",
-        url:getUserProfileByIdURL,
-        dataType: "json",
-        "headers": {
-		    	"Content-Type": "application/json",
-				"Authorization": accessToken	
-		  	},
-			success: function (data) {
-				checkSession(data.success);
-					var obj = data.body;
-					$('#topNavuserName').append(obj.firstName+' '+obj.lastName);
-            }
-      });
-	
+		type: "GET",
+		url: getUserProfileByIdURL,
+		dataType: "json",
+		"headers": {
+			"Content-Type": "application/json",
+			"Authorization": accessToken
+		},
+		success: function(data) {
+			checkSession(data.success);
+			var obj = data.body;
+			$('#topNavuserName').append(obj.firstName + ' ' + obj.lastName);
+		}
+	});
+
 };
 
-function roleBaseAccess(){
-	if(userRole.toLowerCase() == 'admin' ){
+function roleBaseAccess() {
+	if (userRole.toLowerCase() == 'admin') {
 		$('.changePasswordTopNav').remove();
-	}else if(userRole.toLowerCase() == 'supervisor' ){
+	} else if (userRole.toLowerCase() == 'supervisor') {
 		$('.adminPanelTopNav').remove();
-	}else if(userRole.toLowerCase() == 'teammember'){
+	} else if (userRole.toLowerCase() == 'teammember') {
 		$('.adminPanelTopNav').remove();
 	}
 
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	topNavUserName();
-	setInterval(function(){ 
-		checkSession(); 
-	}, 61000);	
+	setInterval(function() {
+		checkSession();
+	}, 300000);
 })// ready ends
--- in/src/main/resources/static/logics/login.js
+++ in/src/main/resources/static/logics/login.js
@@ -1,34 +1,34 @@
-$(document).ready(function(){
- // on click Sign In Button checks with the remote server that username =='admin' and password == 'password'
-    $("#login").click(function(e){
+$(document).ready(function() {
+	// on click Sign In Button checks with the remote server that username =='admin' and password == 'password'
+	$("#login").click(function(e) {
 		e.preventDefault();
 		var settings = {
-		  "url": postLoginURL,
-		  "method": "POST",
-		  "timeout": 0,
-		  "headers": {
-		    "Content-Type": "application/json",
-		  },
-		  "data": JSON.stringify({
-		    "userName": $('#email').val(),
-		    "password": $('#password').val()
-		  }),
+			"url": postLoginURL,
+			"method": "POST",
+			"timeout": 0,
+			"headers": {
+				"Content-Type": "application/json",
+			},
+			"data": JSON.stringify({
+				"userName": $('#email').val(),
+				"password": $('#password').val()
+			}),
 		};
-		
-		$.ajax(settings).done(function (response) {
-	
-			if(response.success == 200){
+
+		$.ajax(settings).done(function(response) {
+
+			if (response.success == 200) {
 				window.location = 'dashboard';
-				Cookies.set('token', response.token, {expires: 1/1440});
+				Cookies.set('token', response.token, { expires: 30 / 1440 });
 				sessionStorage.setItem('token', response.token);
 				sessionStorage.setItem('roles', response.body)
-			}else if(response.success == 500){
+			} else if (response.success == 500) {
 				alert('Either user is not registered or there is some other issue...');
-			}else{
+			} else {
 				alert('try again');
 			}
-		
-    });
 
-});
+		});
+
+	});
 });
