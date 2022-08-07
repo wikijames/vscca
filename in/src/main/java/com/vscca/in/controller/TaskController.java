5ac65cff9bd5770c954bac21444447c155fbd621
manu.kumar <manu.kumar@globalsign.com>
2022-08-07 23:05:24 +0530

    task controller chnage

--- in/src/main/java/com/vscca/in/controller/TaskController.java
+++ in/src/main/java/com/vscca/in/controller/TaskController.java
@@ -170,6 +170,12 @@ public class TaskController {
 			taskUserDetails.setIntimation(taskDto.getIntimation());
 			taskStatus.setTaskId(taskId.getId());
 			taskStatus.setStatus("In Process");
+			try {
+				taskStatus.setEndDate(formatter.parse(formatter.format(date)));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			taskUserDetailsService.save(taskUserDetails);
 			taskStatusService.save(taskStatus);
 			response.setSuccess(200);
