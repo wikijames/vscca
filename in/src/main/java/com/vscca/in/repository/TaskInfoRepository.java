a15c52b6aeffbadbf20adca0de488824a3f1660e
wikijames <sharmavivek.1989@gmail.com>
2026-04-04 10:26:40 +0530

    update task info visibility less then 60 days for users

--- in/src/main/java/com/vscca/in/repository/TaskInfoRepository.java
+++ in/src/main/java/com/vscca/in/repository/TaskInfoRepository.java
@@ -22,19 +22,19 @@ public interface TaskInfoRepository extends JpaRepository<TaskInfo, Long> {
 	@Transactional
 	void deleteById(Long id);
 	
-	@Query(value="select ts.task_id, ti.project_name,ti.party_name,ti.weightage,ti.task_description,ti.task_type,ti.billing_client,ti.created_at,ti.due_date,ts.status,ts.delay_reason,ts.remarks,ts.end_date,tud.responsibility,tud.intimation,tud.exceution,tud.consulting from vscca.task_status as ts, vscca.task_info as ti ,vscca.task_user_details as tud,(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and ts.status not in ('Done') order by ti.due_date asc ",nativeQuery=true)
+	@Query(value="select ts.task_id, ti.project_name,ti.party_name,ti.weightage,ti.task_description,ti.task_type,ti.billing_client,ti.created_at,ti.due_date,ts.status,ts.delay_reason,ts.remarks,ts.end_date,tud.responsibility,tud.intimation,tud.exceution,tud.consulting from vscca.task_status as ts, vscca.task_info as ti ,vscca.task_user_details as tud,(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and ts.status not in ('Done') and ti.due_date <= DATE_ADD(CURDATE(), INTERVAL 59 DAY) order by ti.due_date asc ",nativeQuery=true)
 	List<Object[]> findTaskDetails();
 
 	@Query(value="select ts.task_id, ti.project_name,ti.party_name,ti.weightage,ti.task_description,ti.task_type,ti.billing_client,ti.created_at,ti.due_date,ts.status,ts.delay_reason,ts.remarks,ts.end_date,tud.responsibility,tud.intimation,tud.exceution,tud.consulting from vscca.task_status as ts, vscca.task_info as ti ,vscca.task_user_details as tud,\r\n"
-			+ "(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and tud.intimation= ? ",nativeQuery=true)
+			+ "(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and tud.intimation= ? and ti.due_date <= DATE_ADD(CURDATE(), INTERVAL 59 DAY) ",nativeQuery=true)
 	List<Object[]> findTaskDetailsIntimation(String emailId);
 
 	@Query(value="select ts.task_id, ti.project_name,ti.party_name,ti.weightage,ti.task_description,ti.task_type,ti.billing_client,ti.created_at,ti.due_date,ts.status,ts.delay_reason,ts.remarks,ts.end_date,tud.responsibility,tud.intimation,tud.exceution,tud.consulting from vscca.task_status as ts, vscca.task_info as ti ,vscca.task_user_details as tud,\r\n"
-     + "(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and tud.exceution= ? ",nativeQuery=true)
+     + "(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and tud.exceution= ? and ti.due_date <= DATE_ADD(CURDATE(), INTERVAL 59 DAY) ",nativeQuery=true)
 	List<Object[]> findTaskDetailsExceution(String emailId);
 	
 	@Query(value="select ts.task_id, ti.project_name,ti.party_name,ti.weightage,ti.task_description,ti.task_type,ti.billing_client,ti.created_at,ti.due_date,ts.status,ts.delay_reason,ts.remarks,ts.end_date,tud.responsibility,tud.intimation,tud.exceution,tud.consulting from vscca.task_status as ts, vscca.task_info as ti ,vscca.task_user_details as tud,\r\n"
-			+ "(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and tud.consulting= ? ",nativeQuery=true)
+			+ "(select max(end_date) as end_date,task_id from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date and ti.id=tud.task_id and ts.task_id=ti.id and tud.consulting= ? and ti.due_date <= DATE_ADD(CURDATE(), INTERVAL 59 DAY) ",nativeQuery=true)
 	List<Object[]> findTaskDetailsConsulting(String emailId);
 	
 
@@ -47,7 +47,7 @@ public interface TaskInfoRepository extends JpaRepository<TaskInfo, Long> {
 			+ "tud.responsibility,tud.intimation,tud.exceution,tud.consulting from vscca.task_status as ts,\r\n"
 			+ " vscca.task_info as ti ,vscca.task_user_details as tud,(select max(end_date) as end_date,task_id \r\n"
 			+ " from vscca.task_status group by task_id) as tid where ts.task_id=tid.task_id and tid.end_date=ts.end_date\r\n"
-			+ " and ti.id=tud.task_id and ts.task_id=ti.id and ? in (tud.responsibility ,tud.intimation ,tud.exceution ,tud.consulting) and ts.status not in ('Done') order by ti.due_date asc ",nativeQuery=true)
+			+ " and ti.id=tud.task_id and ts.task_id=ti.id and ? in (tud.responsibility ,tud.intimation ,tud.exceution ,tud.consulting) and ts.status not in ('Done') and ti.due_date <= DATE_ADD(CURDATE(), INTERVAL 59 DAY) order by ti.due_date asc ",nativeQuery=true)
 	List<Object[]> findTaskDetailsByUser(String emailId);
 	
 	@Query(value="select ts.task_id, ti.project_name,ti.party_name,ti.weightage,ti.task_description,ti.task_type,\r\n"
